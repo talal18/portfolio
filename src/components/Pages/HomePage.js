6a3625f58e8755300d15c1c02411baf29b4e7f36
talal <talal_kasem@hotmail.com>
2019-02-17 16:23:50 -0500

    added intro

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -7,7 +7,18 @@ export default class HomePage extends Component {
         <div className="jumbotron">
           <div className="container text-center">
             <h1>Talal Qasem</h1>
-            <p>Some text that represents "Me"...</p>
+            <p>
+              I'm an experienced Web Developer with a demonstrated history in
+              cross-platform web application development primarily in React
+              Native. I love solving problems and creating new applications
+              using React, JavaScript and it's frameworks, HTML5 and CSS,
+              Bootstrap and PHP. I have a real passion for emerging technologies
+              and I have demonstrated my enthusiasm in software and web
+              development by continually learning after graduating with honors
+              from Computer Programming from Algonquin College of Applied Arts
+              and Technology. I am looking for exciting opportunities in a
+              professional environment which I can grow and contribute.
+            </p>
           </div>
         </div>
 
